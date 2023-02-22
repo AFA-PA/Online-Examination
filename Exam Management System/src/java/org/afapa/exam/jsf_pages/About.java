bc2f8ecdb81ae9ccd29d1c950d4ba66d16c12bae
Ketish <abdullaht1155@gmail.com>
2023-02-22 14:55:57 +0300

    added member list

---
+++ Exam Management System/src/java/org/afapa/exam/jsf_pages/About.java
@@ -0,0 +1,42 @@
+package org.afapa.exam.jsf_pages;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+
+@Named("about")
+@ApplicationScoped
+public class About implements Serializable {
+
+    public static final Member ABUZER = new Member("Abuzer", "Nasir", "ugr/22553/13");
+    public static final Member ABDULAHI = new Member("Abdulahi", "Taju", "ugr/22517/13");
+    public static final Member PETROS = new Member("Petros", "Lakew", "ugr/22596/13");
+    public static final Member ABDELA = new Member("Abdela", "Nuru", "ugr/22561/13");
+    public static final Member FASIKA = new Member("Fasika", "Zergaw", "ugr/23653/13");
+
+    public static final Member[] members = new Member[]{
+        About.ABUZER, About.ABDULAHI, About.PETROS, About.ABDELA, About.FASIKA
+    };
+
+    @Setter
+    @Getter
+    public static class Member {
+        String firstName;
+        String lastName;
+        String id;
+
+        public Member(String firstName, String lastName, String id) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+        }
+    }
+
+    public List<Member> getMembers() {
+        return Arrays.asList(members);
+    }
+}
